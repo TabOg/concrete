4d30c2ca1aaa1c433f6dcbe25f1080bed2e57256
Antoniu Pop <antoniu.pop@zama.ai>
2024-03-19 14:13:35 +0000

    mark encode loops parallel

--- compilers/concrete-compiler/compiler/lib/Conversion/ExtractSDFGOps/ExtractSDFGOps.cpp
+++ compilers/concrete-compiler/compiler/lib/Conversion/ExtractSDFGOps/ExtractSDFGOps.cpp
@@ -294,6 +294,15 @@ struct ExtractSDFGOpsPass : public ExtractSDFGOpsBase<ExtractSDFGOpsPass> {
     }
 
     (void)mlir::simplifyRegions(rewriter, func->getRegions());
+
+      func.walk([&](mlir::scf::ForOp loop) {
+      for (mlir::Operation &op : loop.getBody()->getOperations()) {
+	if (llvm::isa<Concrete::EncodeExpandLutForBootstrapTensorOp>(op)) {
+	  loop->setAttr("parallel", rewriter.getBoolAttr(true));
+	  return;
+	}
+      }
+    });
   }
 };
 } // namespace
